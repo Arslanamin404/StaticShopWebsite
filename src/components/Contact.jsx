c6e5c971a26b58fce0db904b37e938416bba7276
Mohammad Arsalan Rather <arslanamin.org@gmail.com>
2024-10-24 21:34:00 +0530

    Owner added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,31 @@
-import { FaLocationDot, FaPhone, FaRegCopyright } from "react-icons/fa6";
+import {
+  FaLocationDot,
+  FaPhone,
+  FaRegCopyright,
+  FaUser,
+} from "react-icons/fa6";
 import { IoIosTime } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
 export const Contact = () => {
   return (
     <div id="contact" className="bg-amber-500 py-10 px-5 md:py-12 md:px-16">
       <h1 className="text-3xl md:text-6xl font-extrabold text-center text-gray-800">
-        Contact Us 
+        Contact Us
       </h1>
       <div className="flex flex-col md:flex-row justify-center items-center gap-10 mt-5 md:mt-10 md:gap-16">
         <div className=" flex items-start justify-center flex-col gap-y-5 md:rounded-lg text-white bg-gray-800 px-5 md:px-10 md:py-8 py-6 ">
           <h1 className=" text-2xl mb-3 md:text-3xl font-bold">
             Visit Our Shop
           </h1>
+          <div className="flex items-center justify-center gap-2">
+            <span>
+              <FaUser size={20} />
+            </span>
+            <p className="md:ml-1 text-sm md:text-xl font-semibold flex items-start justify-center flex-col">
+              <span>Mohammad Amin Rather (peer),</span>
+              <span>Younis Rasheed</span>
+            </p>
+          </div>
           <div className="flex items-center justify-center gap-2">
             <span>
               <FaPhone size={20} />
